623b416054e693acf7e3162f7f25fe7ac1918ddf
abrar <abrarhameed911@gmail.com>
2024-07-12 22:19:36 +0500

    initial commit

---
+++ Controller.php
@@ -0,0 +1,90 @@
+<?php
+
+session_start();
+class users
+{
+
+
+
+    private $conn;
+
+    public function __construct()
+    {
+
+
+        $this->conn = new mysqli("localhost", "root", "", "newdb");
+
+
+
+        if ($this->conn->connect_error) {
+
+            die("connection error  " . $this->conn->connect_error);
+        } else {
+           // echo "connection succcesful";
+        }
+    }
+
+
+    public function add($request)
+    {
+
+        $name = $request['name'];
+        $email = $request['email'];
+        $comment = $request['comment'];
+
+        $query1 = "insert into users(name,email,comment) VALUES('$name','$email','$comment')";
+        $this->conn->query($query1);
+    }
+
+
+
+
+    public function view()
+    {
+
+
+        $query2 = "select * from users where 1";
+
+        $result = $this->conn->query($query2);
+
+        if ($result->num_rows > 0) {
+
+            $data = [];
+
+            while ($row = $result->fetch_assoc()) {
+
+
+                $data[] = $row;
+            }
+
+            $_SESSION['data'] = $data;
+            header('location:OutPut.php');
+        }
+    }
+}
+
+
+
+
+
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+
+    if (isset($_POST['submit'])) {
+
+        $user = new users;
+
+        $user->add($_REQUEST);
+    } elseif (isset($_POST['view'])) {
+
+
+        $user = new users;
+        $user->view();
+    }
+
+
+
+
+
+
+}
---
+++ OutPut.php
@@ -0,0 +1,49 @@
+ <?php
+   session_start();
+
+   $data = $_SESSION['data'];
+   ?>
+
+ <!DOCTYPE html>
+ <html lang="en">
+
+ <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+    <style>
+       .table th,
+       td {
+
+          border: 1px solid black;
+
+
+
+       }
+    </style>
+ </head>
+
+ <body>
+    <table>
+       <tr>
+          <th>nme</th>
+          <th>email</th>
+          <th>comment</th>
+       </tr>
+
+       <?php foreach ($data as $row) { ?>
+          <tr>
+             <td> <?php echo $row['name'];  ?> </td>
+
+             <td> <?php echo $row['email'];  ?> </td>
+
+             <td> <?php echo $row['comment'];  ?> </td>
+
+          <?php  } ?>
+          </tr>
+    </table>
+
+ </body>
+
+ </html>
